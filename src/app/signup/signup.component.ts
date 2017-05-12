7855a3f930d3c8c056d45b6e79b55fb2c51dd0e1
opaluwa john <johnopaluwa@gmail.com>
2017-05-12 22:31:37 +0100

    update on the signup

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -22,11 +22,18 @@ export class SignupComponent {
   }
 
   register() {
-    db.User.register(this.user.name, this.user.password).then(() => {
-      this.router.navigate(['/me']);
-    }, (error) => {
-      this.error = error.message;
-    });
+
+
+    var file = new db.File({path: '/Images/blank.png'});
+      var user = new db.User({
+        'username': this.user.name,
+        'image': file,
+      });
+      db.User.register(user, this.user.password).then(() => {
+        this.router.navigate(['/me']);
+      }, (error) => {
+        this.error = error.message;
+      });
   }
 
   logIn() {
